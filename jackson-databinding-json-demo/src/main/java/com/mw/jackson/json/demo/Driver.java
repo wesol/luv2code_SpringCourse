8989f9b0f5946a6eb4fc1658eeef08448d08777f
Mateusz Wesołowski <mwesolowski88@gmail.com>
2019-12-08 14:07:30 +0100

    Added content from sections 56-61

---
+++ jackson-databinding-json-demo/src/main/java/com/mw/jackson/json/demo/Driver.java
@@ -0,0 +1,37 @@
+package com.mw.jackson.json.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+public class Driver {
+    public static void main(String[] args) {
+        try {
+            //create object mapper
+            ObjectMapper mapper = new ObjectMapper();
+
+            //read JSON file and map/convert to Java POJO
+            //data/sample-lite.json
+            Student student = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+
+            //print first name and last name
+            System.out.println("First name: " + student.getFirstName());
+            System.out.println("Last name: " + student.getLastName());
+
+            Address address = student.getAddress();
+
+            System.out.println("Street: " + address.getStreet());
+            System.out.println("Country: " + address.getCountry());
+            System.out.println("City: " + address.getCity());
+            System.out.println("State: " + address.getState());
+
+            for (String s : student.getLanguages()){
+                System.out.println(s);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ spring-rest-demo/src/main/java/com/mw/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,12 @@
+package com.mw.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.mw.springdemo")
+public class DemoAppConfig {
+
+}
