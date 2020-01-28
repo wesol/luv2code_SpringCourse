9115350c4604077ce87b4ac31d439593cbb6560d
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-01-28 20:51:19 +0100

    Fixed with problem with "" number (used Integer and @Not nul)

--- spring-mvc-demo/src/com/mw/springdemo/model/Customer.java
+++ spring-mvc-demo/src/com/mw/springdemo/model/Customer.java
@@ -10,9 +10,10 @@ public class Customer {
     @Size(min = 1, max = 32, message = "Is required")
     private String lastName;
 
+    @NotNull(message = "Is required")
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 10, message = "must be less than or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -34,11 +35,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
