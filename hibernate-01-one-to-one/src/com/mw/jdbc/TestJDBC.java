fa1dd136a2e956f202261bfe5d6d5ab63edef928
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-02-06 23:05:32 +0100

    Added new project for @OneToOne
    (copy from hibernate tutorial and removed unneeded elements)

---
+++ hibernate-01-one-to-one/src/com/mw/jdbc/TestJDBC.java
@@ -0,0 +1,23 @@
+package com.mw.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJDBC {
+    public static void main(String[] args) {
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=CET";
+        String user = "hbstudent";
+        String password = "hbstudent";
+
+        System.out.println("Connecting to database: " + jdbcUrl);
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, user, password);
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
