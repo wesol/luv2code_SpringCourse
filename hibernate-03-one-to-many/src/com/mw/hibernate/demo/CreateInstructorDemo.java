a141815bb359824b68a95777140cd75458d7e9a0
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-02-08 18:18:26 +0100

    Renamed and modified the class which creating instructors

--- hibernate-03-one-to-many/src/com/mw/hibernate/demo/CreateInstructorDemo.java
+++ hibernate-03-one-to-many/src/com/mw/hibernate/demo/CreateInstructorDemo.java
@@ -1,12 +1,13 @@
 package com.mw.hibernate.demo;
 
+import com.mw.hibernate.demo.entity.Course;
 import com.mw.hibernate.demo.entity.Instructor;
 import com.mw.hibernate.demo.entity.InstructorDetail;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class CreateDemo {
+public class CreateInstructorDemo {
 
     public static void main(String[] args) {
 
@@ -14,19 +15,20 @@ public class CreateDemo {
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
                                                     .addAnnotatedClass(Instructor.class)
                                                     .addAnnotatedClass(InstructorDetail.class)
+                                                    .addAnnotatedClass(Course.class)
                                                     .buildSessionFactory();
 
         // create session
         Session session = factory.getCurrentSession();
 
         try {
+            Instructor instructor = new Instructor("Chad", "Darby", "darby@luvtocode.com");
             InstructorDetail instructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "Luv 2 code!!!");
-            Instructor instructor = new Instructor("Chad1", "Darby", "darby@luvtocode.com");
 
-            instructorDetail.setInstructor(instructor);
+            instructor.setInstructorDetail(instructorDetail);
 
             session.beginTransaction();
-            session.save(instructorDetail);
+            session.save(instructor);
             session.getTransaction().commit();
 
             System.out.println("Done");
