881a8c1f9344ff2fb8201b40e0ebdd75eec0d0c4
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-02-08 19:26:40 +0100

    Added class to retrieve courses

---
+++ hibernate-03-one-to-many/src/com/mw/hibernate/demo/GetInstructorCoursesDemo.java
@@ -0,0 +1,38 @@
+package com.mw.hibernate.demo;
+
+import com.mw.hibernate.demo.entity.Course;
+import com.mw.hibernate.demo.entity.Instructor;
+import com.mw.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorCoursesDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                                                    .addAnnotatedClass(Instructor.class)
+                                                    .addAnnotatedClass(InstructorDetail.class)
+                                                    .addAnnotatedClass(Course.class)
+                                                    .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+            Instructor instructor = session.get(Instructor.class, 1);
+            System.out.println("Instructor courses: " + instructor.getCourses());
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
