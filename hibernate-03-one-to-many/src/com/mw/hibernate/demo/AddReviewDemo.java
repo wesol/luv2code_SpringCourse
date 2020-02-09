9fde269813730a3a5e3a43fa70eff70a61644563
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-02-09 11:12:45 +0100

    Added class to adding a review

---
+++ hibernate-03-one-to-many/src/com/mw/hibernate/demo/AddReviewDemo.java
@@ -0,0 +1,44 @@
+package com.mw.hibernate.demo;
+
+import com.mw.hibernate.demo.entity.Course;
+import com.mw.hibernate.demo.entity.Instructor;
+import com.mw.hibernate.demo.entity.InstructorDetail;
+import com.mw.hibernate.demo.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class AddReviewDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                                                    .addAnnotatedClass(Instructor.class)
+                                                    .addAnnotatedClass(InstructorDetail.class)
+                                                    .addAnnotatedClass(Course.class)
+                                                    .addAnnotatedClass(Review.class)
+                                                    .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            Instructor instructor;
+
+            session.beginTransaction();
+            Course course = session.get(Course.class, 10);
+            course.addReview(new Review("Great course, I play guitar like Satriani"));
+
+            session.save(course);
+            session.getTransaction().commit();
+
+            System.out.println("Done");
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
