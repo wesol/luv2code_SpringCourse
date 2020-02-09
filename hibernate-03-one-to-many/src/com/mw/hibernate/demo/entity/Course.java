c99456dac87cff9f0dec22adfcca4103696ea8cf
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-02-09 10:51:19 +0100

    Added review entity

--- hibernate-03-one-to-many/src/com/mw/hibernate/demo/entity/Course.java
+++ hibernate-03-one-to-many/src/com/mw/hibernate/demo/entity/Course.java
@@ -2,6 +2,8 @@ package com.mw.hibernate.demo.entity;
 
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "course")
@@ -18,6 +20,10 @@ public class Course {
     @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     public Course() {
     }
@@ -48,6 +54,22 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public void addReview(Review review){
+        if (reviews == null){
+            reviews = new ArrayList<>();
+        }
+
+        reviews.add(review);
     }
 
     @Override
