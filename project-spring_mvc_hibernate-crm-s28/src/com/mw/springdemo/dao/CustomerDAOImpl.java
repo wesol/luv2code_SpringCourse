171b32715da0e5abf1c4141ebe69d25244e2ebb5
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-02-24 21:02:55 +0100

    Simplified class

--- project-spring_mvc_hibernate-crm-s28/src/com/mw/springdemo/dao/CustomerDAOImpl.java
+++ project-spring_mvc_hibernate-crm-s28/src/com/mw/springdemo/dao/CustomerDAOImpl.java
@@ -23,9 +23,8 @@ public class CustomerDAOImpl implements CustomerDAO {
         Session session = sessionFactory.getCurrentSession();
 
         Query<Customer> query = session.createQuery("from Customer", Customer.class);
-        List<Customer> customersList = customersList = query.getResultList();
 
-        return customersList;
+        return query.getResultList();
     }
 
 
