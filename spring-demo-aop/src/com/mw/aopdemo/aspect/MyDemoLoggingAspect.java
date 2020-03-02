43e7015792ab7d221429e37324bd56ccd397ac6b
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-03-02 23:06:37 +0100

    Added @Pointcut - and taken off handling addAccount from @Before

--- spring-demo-aop/src/com/mw/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/mw/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,15 +3,17 @@ package com.mw.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-    // this is where we add all of out related advices for logging
+    @Pointcut("execution(public void addAccount())")
+    public  void anyNameMethod() {}
 
-    @Before("execution(public void addAccount())")
+    @Before("anyNameMethod()")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n =====>>>> Executing @Before advice on addAccount()");
