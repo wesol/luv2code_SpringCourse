1145e9d1f9ad09a4fdc971569b23633ac6c41d0a
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-03-07 20:10:14 +0100

    Added @Before and @AfterReturning to mvc

--- project-spring_mvc_hibernate-crm-s28/src/com/mw/springdemo/aspect/CRMLoggingAspect.java
+++ project-spring_mvc_hibernate-crm-s28/src/com/mw/springdemo/aspect/CRMLoggingAspect.java
@@ -1,6 +1,9 @@
 package com.mw.springdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -15,15 +18,41 @@ public class CRMLoggingAspect {
 
     // setup pointcut declarations
     @Pointcut("execution(* com.mw.springdemo.controller.*.*(..))")
-    private void forControllerPackage() {}
+    private void forControllerPackage() {
+    }
 
     @Pointcut("execution(* com.mw.springdemo.service.*.*(..))")
-    private void forServicePackage() {}
+    private void forServicePackage() {
+    }
 
     @Pointcut("execution(* com.mw.springdemo.dao.*.*(..))")
-    private void forDaoPackage() {}
+    private void forDaoPackage() {
+    }
 
     @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
-    private void forAppFlow(){}
+    private void forAppFlow() {
+    }
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint) {
+
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("========>> In @Before, calling method: " + method);
+
+        Object[] args = joinPoint.getArgs();
+
+        for (Object o : args) {
+            logger.info("--- argument: " + o);
+        }
+    }
+
+    @AfterReturning(pointcut = "forAppFlow()",
+                    returning = "result")
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("========>> In @AfterReturning, calling method: " + method);
+        logger.info("========>> Result: " + result);
+    }
 
 }
