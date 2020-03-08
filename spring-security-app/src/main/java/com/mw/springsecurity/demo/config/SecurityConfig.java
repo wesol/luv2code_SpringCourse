ea6218fffcaac1ac857a3a6f510e6fb4b9b4035b
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-03-08 16:01:23 +0100

    Override the configure method

--- spring-security-app/src/main/java/com/mw/springsecurity/demo/config/SecurityConfig.java
+++ spring-security-app/src/main/java/com/mw/springsecurity/demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.mw.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,12 +16,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-       User.UserBuilder users = User.withDefaultPasswordEncoder();
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
 
-       auth.inMemoryAuthentication()
-           .withUser(users.username("employee").password("test").roles("EMPLOYEE"))
-           .withUser(users.username("manager").password("test").roles("MANAGER"))
-           .withUser(users.username("admin").password("test").roles("ADMIN"));
+        auth.inMemoryAuthentication()
+            .withUser(users.username("employee").password("test").roles("EMPLOYEE"))
+            .withUser(users.username("manager").password("test").roles("MANAGER"))
+            .withUser(users.username("admin").password("test").roles("ADMIN"));
 
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+            .and()
+            .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll();
     }
 }
