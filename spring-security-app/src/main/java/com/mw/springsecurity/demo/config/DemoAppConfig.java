346933c7c9ddf0c0c34aa8e9bc09683bf44a255d
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-03-22 13:30:23 +0100

    Configured data source

--- spring-security-app/src/main/java/com/mw/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-app/src/main/java/com/mw/springsecurity/demo/config/DemoAppConfig.java
@@ -1,17 +1,32 @@
 package com.mw.springsecurity.demo.config;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Objects;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.mw.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+    @Autowired
+    private Environment env;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     @Bean
     public ViewResolver viewResolver() {
@@ -22,5 +37,48 @@ public class DemoAppConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+    // define Bean for security data source
+    @Bean
+    public DataSource securityDataSource() {
+
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
+
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    private int getIntProperty(String propName) {
+
+        String propVal = env.getProperty(propName);
+
+        int propValueInt = 0;
+        try {
+            propValueInt = Integer.parseInt(Objects.requireNonNull(propVal));
+        } catch (NumberFormatException e) {
+            logger.severe("Problem with: \"" + propName + "\" property");
+            e.printStackTrace();
+        }
+
+        return propValueInt;
     }
 }
