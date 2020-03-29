7ec1318163053a741c61c6a9f7c778548f582ed1
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-03-29 13:41:02 +0200

    Added config classes

---
+++ s58-springrest/src/main/java/com/mw/springdemo/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.mw.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.mw.springdemo")
+public class AppConfig implements WebMvcConfigurer {
+
+
+}
+
+
+
+
+
+
+
+
+
