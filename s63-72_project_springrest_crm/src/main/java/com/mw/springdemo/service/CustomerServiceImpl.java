74596c81de167ddc18a912c223214d94363b9f3f
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-03-30 18:41:57 +0200

    Added entity, service, sql to handling rest app (from ealier projects)

---
+++ s63-72_project_springrest_crm/src/main/java/com/mw/springdemo/service/CustomerServiceImpl.java
@@ -0,0 +1,50 @@
+package com.mw.springdemo.service;
+
+import java.util.List;
+
+import com.mw.springdemo.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mw.springdemo.dao.CustomerDAO;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// need to inject customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+	@Override
+	@Transactional
+	public void saveCustomer(Customer theCustomer) {
+
+		customerDAO.saveCustomer(theCustomer);
+	}
+
+	@Override
+	@Transactional
+	public Customer getCustomer(int theId) {
+		
+		return customerDAO.getCustomer(theId);
+	}
+
+	@Override
+	@Transactional
+	public void deleteCustomer(int theId) {
+		
+		customerDAO.deleteCustomer(theId);
+	}
+}
+
+
+
+
+
