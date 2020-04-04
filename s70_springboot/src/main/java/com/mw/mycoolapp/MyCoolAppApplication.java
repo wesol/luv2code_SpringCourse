74ebfd948f50dc3f3022c134807050c06e1c936c
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-04-04 14:50:11 +0200

    Added rest controller and refactored name of main class

--- s70_springboot/src/main/java/com/mw/mycoolapp/MyCoolAppApplication.java
+++ s70_springboot/src/main/java/com/mw/mycoolapp/MyCoolAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MycoolappApplication {
+public class MyCoolAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MycoolappApplication.class, args);
+		SpringApplication.run(MyCoolAppApplication.class, args);
 	}
 
 }
