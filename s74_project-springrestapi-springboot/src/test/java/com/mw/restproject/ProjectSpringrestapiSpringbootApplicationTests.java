55a36194fad53087ffeefbbdb5be832a86020631
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-04-05 12:44:23 +0200

    Added new project with spring boot initializr and sql

---
+++ s74_project-springrestapi-springboot/src/test/java/com/mw/restproject/ProjectSpringrestapiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mw.restproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringrestapiSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
