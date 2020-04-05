cc43e63679c5005efa8a08dfc68c1e6f34cd51bb
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-04-05 15:16:16 +0200

    Added empty post, put, delete methods for employees

--- s74_project-springrestapi-springboot/src/main/java/com/mw/restproject/rest/EmployeeRestController.java
+++ s74_project-springrestapi-springboot/src/main/java/com/mw/restproject/rest/EmployeeRestController.java
@@ -3,8 +3,7 @@ package com.mw.restproject.rest;
 import com.mw.restproject.entity.Employee;
 import com.mw.restproject.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,11 +13,37 @@ public class EmployeeRestController {
     @Autowired
     private EmployeeService employeeService;
 
+
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
 
         return employeeService.findAll();
     }
 
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeByID(@PathVariable long employeeId) {
 
+
+        return null;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(Employee employee) {
+
+
+        return null;
+    }
+
+    @PutMapping("/employees")
+    public String updateEmployee(Employee employee) {
+
+
+        return null;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable long employeeId) {
+
+        return null;
+    }
 }
