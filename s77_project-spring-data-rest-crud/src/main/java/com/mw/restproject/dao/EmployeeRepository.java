50a8022773419edcd32833c48c04c1ccd9027aa9
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-04-08 22:47:14 +0200

    Changed default endpoint

--- s77_project-spring-data-rest-crud/src/main/java/com/mw/restproject/dao/EmployeeRepository.java
+++ s77_project-spring-data-rest-crud/src/main/java/com/mw/restproject/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.mw.restproject.dao;
 
 import com.mw.restproject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
