d124cef0b4c008d457a7ec7fb7d235daebbe167d
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-04-15 00:16:32 +0200

    Added employees updating

--- s80-springboot-thymeleaf-project/src/main/java/com/mw/springbootthymeleaf/controller/EmployeeController.java
+++ s80-springboot-thymeleaf-project/src/main/java/com/mw/springbootthymeleaf/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.mw.springbootthymeleaf.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -38,6 +35,16 @@ public class EmployeeController {
     public String showFormForAdd(Model theModel) {
 
         Employee employee = new Employee();
+
+        theModel.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int employeeId, Model theModel) {
+
+        Employee employee = employeeService.findById(employeeId);
 
         theModel.addAttribute("employee", employee);
 
