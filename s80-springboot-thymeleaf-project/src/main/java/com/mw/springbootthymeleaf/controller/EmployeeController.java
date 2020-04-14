6e4ebe063359a45ca8af290a989a137f15074c5a
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-04-14 23:05:44 +0200

    Moved list-employees template

--- s80-springboot-thymeleaf-project/src/main/java/com/mw/springbootthymeleaf/controller/EmployeeController.java
+++ s80-springboot-thymeleaf-project/src/main/java/com/mw/springbootthymeleaf/controller/EmployeeController.java
@@ -29,7 +29,7 @@ public class EmployeeController {
         // add to the spring model
         theModel.addAttribute("employees", employees);
 
-        return "list-employees";
+        return "employees/list-employees";
     }
 }
 
