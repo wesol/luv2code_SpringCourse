c3d9a70f94b8f925683ed14ac05bd7ff0460a54b
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-04-14 23:24:44 +0200

    Added form and its handlong to adding employee

--- s80-springboot-thymeleaf-project/src/main/java/com/mw/springbootthymeleaf/controller/EmployeeController.java
+++ s80-springboot-thymeleaf-project/src/main/java/com/mw/springbootthymeleaf/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,5 +33,24 @@ public class EmployeeController {
 
         return "employees/list-employees";
     }
+
+    @GetMapping("/showFormAdd")
+    public String showFormForAdd(Model theModel) {
+
+        Employee employee = new Employee();
+
+        theModel.addAttribute("employee", employee);
+
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String showFormForAdd(@ModelAttribute("employee") Employee employee) {
+
+        employeeService.add(employee);
+
+        return "redirect:/employees/list";
+    }
+
 }
 
